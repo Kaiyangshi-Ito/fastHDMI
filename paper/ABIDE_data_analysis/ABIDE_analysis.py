2da2125d17e288be08195ef808c84f909c294c22
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-01-16 13:01:31 -0500

    commit

--- paper/ABIDE_data_analysis/ABIDE_analysis.py
+++ paper/ABIDE_data_analysis/ABIDE_analysis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import numpy as np
@@ -18,12 +18,12 @@ import fastHDMI as mi
 
 csv_file = r"/home/kyang/projects/def-cgreenwo/abide_data/abide_fs60_vout_fwhm0_lh_SubjectIDFormatted_N1050_nonzero_withSEX.csv"
 abide = pd.read_csv(csv_file, encoding='unicode_escape', engine="c")
-abide = dd.read_csv(csv_file, sample=1250000)
+# abide = dd.read_csv(csv_file, sample=1250000)
 
 _abide_name = abide.columns.tolist()[1:]
-_abide_name = list(abide.columns)[1:]
+# _abide_name = list(abide.columns)[1:]
 
-print(_abide_name)
+# print(_abide_name)
 
 abide_name = [_abide_name[-1]] + _abide_name[1:-3]
 # so that the left first column is the outcome and the rest columns are areas
@@ -43,12 +43,20 @@ np.save(r"/home/kyang/ABIDE_Pearson_output", pearson_output)
 
 # # Plots
 
-# In[20]:
+# In[4]:
 
 
 abide_mi = np.load(r"./ABIDE_MI_output.npy")
-print(np.sum(abide_mi > .05))
-plt.hist(np.log(abide_mi), 100)
+plt.hist(np.log(abide_mi), 500)
+plt.show()
+
+
+# In[ ]:
+
+
+abide_pearson = np.load(r"./ABIDE_Pearson_output.npy")
+plt.hist(np.log(abide_pearson), 500)
+plt.show()
 
 
 # In[ ]:
