7d73a4d787ea150f7b802c37d9a5a1df869a3284
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-01-15 23:44:18 -0500

    add ABIDE analysis codes

---
+++ paper/ABIDE_data_analysis/ABIDE_analysis.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+from dask import dataframe as dd
+import matplotlib.pyplot as plt
+import fastHDMI as mi
+
+
+# # Calculate MI for ABIDE data (besides sex and age as they will be included regardless)
+
+# In[ ]:
+
+
+csv_file = r"/home/kyang/projects/def-cgreenwo/abide_data/abide_fs60_vout_fwhm0_lh_SubjectIDFormatted_N1050_nonzero_withSEX.csv"
+abide = pd.read_csv(csv_file, encoding='unicode_escape', engine="pyarrow")
+#abide = dd.read_csv(csv_file)
+
+_abide_name = abide.columns.tolist()  #[1:]
+#_abide_name = list(abide.columns)  #[1:]
+
+print(_abide_name)
+
+abide_name = [_abide_name[-1]] + _abide_name[1:-3]
+# so that the left first column is the outcome and the rest columns are areas
+
+mi_output = mi.binary_filter_csv_parallel(csv_file,
+                                          _usecols=abide_name,
+                                          csv_engine="pyarrow")
+np.save(r"/home/kyang/ABIDE_MI_output", mi_output)
+
+pearson_output = mi.Pearson_filter_csv_parallel(csv_file,
+                                                _usecols=abide_name,
+                                                csv_engine="pyarrow")
+np.save(r"/home/kyang/ABIDE_Pearson_output", pearson_output)
+
+
+# # Plots
+
+# In[20]:
+
+
+abide_mi = np.load(r"./ABIDE_MI_output.npy")
+print(np.sum(abide_mi > .05))
+plt.hist(np.log(abide_mi), 100)
+
+
+# In[ ]:
+
+
+
+
---
+++ paper/ABIDE_data_analysis/ABIDE_analysis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=def-masd
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=80G
+#SBATCH --time=2-00:00:00
+#SBATCH --job-name=ABIDE
+
+module load gcc llvm rust arrow cuda nodejs python/3.8.10
+
+virtualenv $HOME/jupyter_py3
+source $HOME/jupyter_py3/bin/activate
+
+python ABIDE_analysis.py
