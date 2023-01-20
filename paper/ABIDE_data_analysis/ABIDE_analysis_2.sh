30a74a382f6c3ed15086fe22730db013d8a3a703
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-01-20 00:10:06 -0500

    commit

--- paper/ABIDE_data_analysis/ABIDE_analysis_2.sh
+++ paper/ABIDE_data_analysis/ABIDE_analysis_2.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-masd
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=80G
-#SBATCH --time=1-00:00:00
+#SBATCH --time=3-00:00:00
 #SBATCH --job-name=ABIDE
 
 module load gcc llvm rust arrow cuda nodejs python/3.8.10
@@ -10,4 +10,4 @@ module load gcc llvm rust arrow cuda nodejs python/3.8.10
 virtualenv $HOME/jupyter_py3
 source $HOME/jupyter_py3/bin/activate
 
-python ABIDE_analysis.py
+python ABIDE_analysis_2.py
