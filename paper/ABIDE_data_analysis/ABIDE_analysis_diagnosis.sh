b9500de19489826c28c13bfd4d7132a88e6dac78
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-01-23 01:51:30 -0500

    commit

--- paper/ABIDE_data_analysis/ABIDE_analysis_diagnosis.sh
+++ paper/ABIDE_data_analysis/ABIDE_analysis_diagnosis.sh
@@ -10,4 +10,7 @@ module load gcc llvm rust arrow cuda nodejs python/3.8.10
 virtualenv $HOME/jupyter_py3
 source $HOME/jupyter_py3/bin/activate
 
+nvidia-smi
+lscpu
+
 python3 ABIDE_analysis_diagnosis.py
