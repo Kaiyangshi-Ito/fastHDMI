002829116722fd08a04595024b3d27af27206e8b
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-02-07 17:52:52 -0500

    add some tasks to test different memory methods

---
+++ paper/ABIDE_data_analysis/ABIDE_analysis_age_high_mem.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+from dask import dataframe as dd
+import matplotlib.pyplot as plt
+from scipy.stats import kendalltau
+from scipy.stats import rankdata
+from scipy.stats import norm
+import fastHDMI as mi
+from sklearn.linear_model import LassoCV
+from sklearn.linear_model import ElasticNetCV
+from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import LarsCV
+from sklearn.linear_model import LassoLarsCV
+from sklearn.metrics import r2_score
+import multiprocess as mp
+from tqdm import tqdm as tqdm
+
+csv_file = r"/home/kyang/projects/def-cgreenwo/abide_data/abide_fs60_vout_fwhm0_lh_SubjectIDFormatted_N1050_nonzero_withSEX.csv"
+# abide = pd.read_csv(csv_file, encoding='unicode_escape', engine="c")
+abide = dd.read_csv(csv_file, sample=1250000)
+
+# _abide_name = abide.columns.tolist()[1:]
+_abide_name = list(abide.columns)[1:]
+
+# print(_abide_name)
+
+# we don't inlcude age and sex in the screening since they should always be included in the model
+abide_name = [_abide_name[-3]] + _abide_name[1:-3]
+
+np.save(r"./ABIDE_columns", _abide_name[1:-3])
+
+# so that the left first column is the outcome and the rest columns are areas
+print("Our developed FFT-based MI calculation:")
+
+mi_output = mi.continuous_screening_csv_parallel(csv_file,
+                                                 _usecols=abide_name,
+                                                 csv_engine="c",
+                                                 sample=1250000,
+                                                 multp=10,
+                                                 core_num=10, share_memory=False)
+np.save(r"./ABIDE_age_MI_output", mi_output)
+
+print("sklearn MI calculation:")
+
+skmi_output = mi.continuous_skMI_screening_csv_parallel(csv_file,
+                                                        _usecols=abide_name,
+                                                        csv_engine="c",
+                                                        sample=1250000,
+                                                        multp=10,
+                                                        core_num=10, share_memory=False)
+np.save(r"./ABIDE_age_skMI_output", skmi_output)
+
+print("Pearson's correlation calculation:")
+
+pearson_output = mi.Pearson_screening_csv_parallel(csv_file,
+                                                   _usecols=abide_name,
+                                                   csv_engine="c",
+                                                   sample=1250000,
+                                                   multp=10,
+                                                   core_num=10, share_memory=False)
+np.save(r"./ABIDE_age_Pearson_output", pearson_output)
--- paper/ABIDE_data_analysis/ABIDE_analysis_diagnosis_dask.sh
+++ paper/ABIDE_data_analysis/ABIDE_analysis_diagnosis_dask.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=80G
-#SBATCH --time=3:00:00
-#SBATCH --job-name=ABIDE_age
+#SBATCH --time=18:00:00
+#SBATCH --job-name=ABIDE_diagnosis
 
 module load gcc llvm rust arrow cuda nodejs python/3.8.10 r/4.0.2 python-build-bundle
 
@@ -32,4 +32,6 @@ pip install --no-index /home/kyang/fastHDMI-1.18.3+computecanada-py3-none-any.wh
 nvidia-smi
 lscpu
 
-python3 ABIDE_analysis_age.py
+cp /home/kyang/projects/def-cgreenwo/abide_data/abide_fs60_vout_fwhm0_lh_SubjectIDFormatted_N1050_nonzero_withSEX.csv $SLURM_TMPDIR/
+
+python3 ABIDE_analysis_diagnosis_dask.py
