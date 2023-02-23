9783b21a07fad130a0a21778ee75ff2498ce0005
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-02-22 23:33:40 -0500

    commit

--- paper/ABIDE_data_analysis/ABIDE_clump.sh
+++ paper/ABIDE_data_analysis/ABIDE_clump.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=80G
 #SBATCH --time=20:00:00
-#SBATCH --job-name=ABIDE_screening_age_high_mem
+#SBATCH --job-name=ABIDE_clump
 
 module load gcc llvm rust arrow cuda nodejs python/3.8.10 r/4.0.2 python-build-bundle
 
