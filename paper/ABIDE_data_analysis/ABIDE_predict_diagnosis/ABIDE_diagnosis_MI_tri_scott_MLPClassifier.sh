28568dae05d1ef353dd996c7ee44c3f4c0b564c5
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-03-03 16:26:26 -0500

    reduce the number of input variables to the predictive modelling

--- paper/ABIDE_data_analysis/ABIDE_predict_diagnosis/ABIDE_diagnosis_MI_tri_scott_MLPClassifier.sh
+++ paper/ABIDE_data_analysis/ABIDE_predict_diagnosis/ABIDE_diagnosis_MI_tri_scott_MLPClassifier.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=80G
-#SBATCH --time=0-22:00:00
+#SBATCH --time=6-12:00:00
 #SBATCH --job-name=diagnosis_MI_tri_scott_MLPClassifier
 
 module load gcc llvm rust arrow cuda nodejs python/3.8.10 r/4.0.2 python-build-bundle
