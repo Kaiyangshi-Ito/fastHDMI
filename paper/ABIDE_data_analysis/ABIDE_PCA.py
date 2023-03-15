2bd3b0173fa1b992e4f501df67f08ac3b8021efb
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-03-15 19:53:20 -0400

    add PCA scripts

---
+++ paper/ABIDE_data_analysis/ABIDE_PCA.py
@@ -0,0 +1,33 @@
+# running PCA on it
+
+import numpy as np
+import pandas as pd
+from dask import dataframe as dd
+import matplotlib.pyplot as plt
+from scipy.stats import kendalltau, rankdata, norm
+import fastHDMI as mi
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler, SplineTransformer
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LassoCV, ElasticNetCV, RidgeCV, LarsCV, LassoLarsCV, LogisticRegressionCV, LinearRegression, LogisticRegression
+from sklearn.neural_network import MLPRegressor, MLPClassifier
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.metrics import r2_score, roc_auc_score
+import multiprocess as mp
+from tqdm import tqdm
+import os
+
+csv_file = os.environ["SLURM_TMPDIR"] + \
+    r"/abide_fs60_vout_fwhm0_lh_SubjectIDFormatted_N1050_nonzero_withSEX.csv"
+abide = pd.read_csv(csv_file, encoding="unicode_escape", engine="c")
+_abide_name = abide.columns.tolist()[1:]
+abide_name = _abide_name[1:-3]
+abide = abide[[abide_name]]
+abide = StandardScaler(copy=False).fit_transform(abide).to_numpy(copy=False)
+
+pca = PCA(n_components=abide.shape[0])
+components = pca.fit(
+    abide
+).components_.T  # transpose because it's a ndarray of shape (n_components, n_features)
+pca_output = abide @ components
+np.save(r"./ABIDE_PCA", pca_output)
