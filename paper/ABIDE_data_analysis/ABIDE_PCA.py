0e734efe4c9ade599ec0003ed437306f561bf237
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-03-16 13:39:20 -0400

    commit

--- paper/ABIDE_data_analysis/ABIDE_PCA.py
+++ paper/ABIDE_data_analysis/ABIDE_PCA.py
@@ -19,8 +19,11 @@ import os
 
 csv_file = os.environ["SLURM_TMPDIR"] + \
     r"/abide_fs60_vout_fwhm0_lh_SubjectIDFormatted_N1050_nonzero_withSEX.csv"
-abide = pd.read_csv(csv_file, encoding="unicode_escape",
-                    engine="c").iloc[:, 2:-3].to_numpy(copy=False)
+abide = pd.read_csv(csv_file, encoding="unicode_escape", engine="c")
+_sex = abide["SEX"].tolist()
+_age = abide["AGE_AT_SCAN"].tolist()
+_diagnosis = abide["DX_GROUP"].tolist()
+abide = abide.iloc[:, 2:-3].to_numpy(copy=False)
 abide = StandardScaler(copy=False).fit_transform(abide)
 
 pca = PCA(n_components=abide.shape[0])
@@ -28,4 +31,9 @@ components = pca.fit(
     abide
 ).components_.T  # transpose because it's a ndarray of shape (n_components, n_features)
 pca_output = abide @ components
-np.save(r"./ABIDE_PCA", pca_output)
+cols = ["PC_" + str(j + 1) for j in range(pca_output.shape[1])]
+df = pd.dataframe(pca_output, columns=cols)
+df["SEX"] = _sex
+df["AGE_AT_SCAN"] = _age
+df["DX_GROUP"] = _diagnosis
+df.to_csv(r"./ABIDE_PCA")
