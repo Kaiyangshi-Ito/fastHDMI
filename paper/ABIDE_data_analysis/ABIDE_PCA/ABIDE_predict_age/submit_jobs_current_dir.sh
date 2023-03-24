a2347e1cad5f5b8b07e21a0c927cdd71e7de233a
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-03-24 00:07:44 -0400

    commit

--- paper/ABIDE_data_analysis/ABIDE_PCA/ABIDE_predict_age/submit_jobs_current_dir.sh
+++ paper/ABIDE_data_analysis/ABIDE_PCA/ABIDE_predict_age/submit_jobs_current_dir.sh
@@ -1,6 +1,30 @@
 #! /bin/bash                                                                    
 
-for x in ./*.sh
+# for x in ./*.sh
+# do
+#         if [ "$x" != "$0" ]
+#         then
+#             sbatch $x
+#         fi
+# done
+
+for x in ./*epa_ISJ*.sh
+do
+        if [ "$x" != "$0" ]
+        then
+            sbatch $x
+        fi
+done
+
+for x in ./*Pearson*.sh
+do
+        if [ "$x" != "$0" ]
+        then
+            sbatch $x
+        fi
+done
+
+for x in ./*skMI*.sh
 do
         if [ "$x" != "$0" ]
         then
