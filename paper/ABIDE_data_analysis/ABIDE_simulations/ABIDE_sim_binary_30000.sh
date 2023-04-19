04a822dbcf2852c295dfb7b1983d0b5799a4a943
Kai Yang <kai.yang2@mail.mcgill.ca>
2023-04-18 20:02:55 -0400

    split the simulations to shorten computing time

---
+++ paper/ABIDE_data_analysis/ABIDE_simulations/ABIDE_sim_binary_30000.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --account=def-masd
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=80G
+#SBATCH --time=20:00:00
+#SBATCH --job-name=binary_30000
+
+module load gcc llvm rust arrow cuda nodejs python/3.8.10 r/4.0.2 python-build-bundle
+
+virtualenv --no-download $SLURM_TMPDIR/env
+source $SLURM_TMPDIR/env/bin/activate
+pip install --no-index --upgrade pip Cython
+
+# ### run this block at the login node to build wheels
+# module load gcc llvm rust arrow cuda nodejs python/3.8.10 r/4.0.2 python-build-bundle
+# ### upgrading the tools
+# pip install --upgrade pip setuptools wheel
+# ### remove all old wheels
+# rm *.whl
+# ### get wheels builder
+# git clone https://github.com/ComputeCanada/wheels_builder
+# export PATH=$PATH:${HOME}/wheels_builder
+# ### build KDEpy 1.1.1
+# ${HOME}/wheels_builder/unmanylinuxize.sh --package KDEpy --version 1.1.1 --python 3.8,3.9,3.10 --find_links https://files.pythonhosted.org/packages/
+# ### built nonconvexAG 1.0.6
+# ${HOME}/wheels_builder/unmanylinuxize.sh --package nonconvexAG --version 1.0.6 --python 3.8,3.9,3.10 --find_links https://files.pythonhosted.org/packages/
+# ### built fastHDMI 1.23.1
+# pip install fastHDMI==1.23.1 --no-cache-dir
+# pip wheel fastHDMI --no-deps
+
+# # Here basically to build the packages at login node and install them in slurm job submission locally
+pip install --no-index bed-reader numpy sklearn matplotlib scipy numba multiprocess scikit-learn cupy rpy2 Cython
+pip install --no-index /home/kyang/KDEpy-1.1.1+computecanada-cp38-cp38-linux_x86_64.whl
+pip install --no-index /home/kyang/nonconvexAG-1.0.6+computecanada-py3-none-any.whl
+pip install --no-index /home/kyang/fastHDMI-1.23.1-cp38-cp38-linux_x86_64.whl
+
+nvidia-smi
+lscpu
+
+echo "running ABIDE_sim_binary_30000.py"
+
+cp /home/kyang/projects/def-cgreenwo/abide_data/abide_fs60_vout_fwhm0_lh_SubjectIDFormatted_N1050_nonzero_withSEX.csv $SLURM_TMPDIR/
+
+python3 ABIDE_sim_binary_30000.py
---
+++ paper/ABIDE_data_analysis/ABIDE_simulations/ABIDE_sim_continuous_40000.py
@@ -0,0 +1,142 @@
+import numpy as np
+import pandas as pd
+from dask import dataframe as dd
+import matplotlib.pyplot as plt
+from scipy.stats import kendalltau, rankdata, norm
+import fastHDMI as mi
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import StandardScaler, SplineTransformer
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LassoCV, ElasticNetCV, RidgeCV, LarsCV, LassoLarsCV, LogisticRegressionCV, LinearRegression, LogisticRegression
+from sklearn.neural_network import MLPRegressor, MLPClassifier
+from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
+from sklearn.metrics import r2_score, roc_auc_score
+import multiprocess as mp
+from tqdm import tqdm
+import os
+import itertools
+
+# read the data
+csv_file = os.environ["SLURM_TMPDIR"] + \
+    r"/abide_fs60_vout_fwhm0_lh_SubjectIDFormatted_N1050_nonzero_withSEX.csv"
+
+abide = pd.read_csv(csv_file, encoding="unicode_escape", engine="c")
+_abide_name = abide.columns.tolist()[1:]
+
+# print(_abide_name)
+
+abide_name = _abide_name[1:-3]
+
+# preserve only the neuro-imaging data
+abide = abide[abide_name]
+abide_original = abide.copy()
+abide_name_original = abide_name.copy()
+
+
+def convert2list(a):
+    b = np.asarray(a)
+    return b.tolist()
+
+
+def sim_based_on_abide_continuous(pair):
+    abide, abide_name = abide_original.copy(), abide_name_original.copy()
+    _num_true_vars, _seed = pair
+    SNR = 3.
+    num_true_vars = _num_true_vars
+    seed = _seed
+    assert num_true_vars < len(abide_name)
+    np.random.seed(seed)
+
+    true_names = np.random.choice(abide_name, num_true_vars, replace=False)
+    true_names = convert2list(true_names)
+
+    true_beta = np.random.uniform(low=1.0, high=2.0,
+                                  size=num_true_vars) * np.random.choice(
+                                      [1., -1.], num_true_vars, replace=True)
+
+    sim_data = abide[true_names].to_numpy(copy=True)
+    sim_data = StandardScaler(copy=False).fit_transform(sim_data)
+    sim_data += np.abs(
+        np.min(sim_data, 0).reshape(1, -1)
+    )  # to ensure that all the the data are positive so we can take square root
+    sim_data = np.sqrt(sim_data)
+    signs = np.random.choice([1., -1.], sim_data.size,
+                             replace=True).reshape(sim_data.shape)
+    sim_data = sim_data * signs
+    sim_data = StandardScaler(copy=False).fit_transform(sim_data)
+
+    X_cov = np.corrcoef(sim_data, rowvar=False)
+    true_sigma_sim = np.sqrt(true_beta.T @ X_cov @ true_beta / SNR)
+
+    outcome = sim_data @ true_beta + np.random.normal(0, true_sigma_sim,
+                                                      sim_data.shape[0])
+
+    abide["outcome"] = outcome
+    abide = abide[["outcome"] + abide_name]
+
+    print("The outcome is continuous.")
+
+    print("Our developed FFT-based MI calculation:")
+
+    try:
+        mi_output = mi.continuous_screening_dataframe_parallel(
+            dataframe=abide,
+            _usecols=["outcome"] + abide_name,
+            multp=10,
+            core_num=32,
+            share_memory=False,
+            kernel="epa",
+            bw="ISJ",
+            norm=2)
+    except:
+        print("The kernel-bw combination reports an error.")
+
+    print("sklearn MI calculation:")
+
+    skmi_output = mi.continuous_skMI_screening_dataframe_parallel(
+        dataframe=abide,
+        _usecols=["outcome"] + abide_name,
+        multp=10,
+        core_num=32,
+        random_state=0,
+        share_memory=False)
+
+    print("Pearson's correlation calculation:")
+
+    pearson_output = mi.Pearson_screening_dataframe_parallel(
+        dataframe=abide,
+        _usecols=["outcome"] + abide_name,
+        multp=10,
+        core_num=32,
+        share_memory=False)
+
+    mi_selection = np.asarray(abide_name)[np.argsort(
+        -mi_output)][:num_true_vars]
+    mi_selection = convert2list(mi_selection)
+    skmi_selection = np.asarray(abide_name)[np.argsort(
+        -skmi_output)][:num_true_vars]
+    skmi_selection = convert2list(skmi_selection)
+    pearson_selection = np.asarray(abide_name)[np.argsort(
+        -pearson_output)][:num_true_vars]
+    pearson_selection = convert2list(pearson_selection)
+
+    mi_sensitivity = len(set(mi_selection)) + len(set(true_names)) - len(
+        set(mi_selection + true_names))
+    mi_sensitivity = mi_sensitivity / len(true_names)
+    skmi_sensitivity = len(set(skmi_selection)) + len(set(true_names)) - len(
+        set(skmi_selection + true_names))
+    skmi_sensitivity = skmi_sensitivity / len(true_names)
+    pearson_sensitivity = len(set(pearson_selection)) + len(
+        set(true_names)) - len(set(pearson_selection + true_names))
+    pearson_sensitivity = pearson_sensitivity / len(true_names)
+    return np.array([mi_sensitivity, skmi_sensitivity, pearson_sensitivity])
+
+
+num_true_vars_list = [40000]
+seed_list = range(100)
+
+itrs = itertools.product(num_true_vars_list, seed_list)
+
+output_array = np.array(list(map(sim_based_on_abide_continuous, tqdm(itrs))))
+output_array = output_array.reshape(1, 100, 3).squeeze()
+np.save(r"./ABIDE_simulations/ABIDE_sim_continuous_40000", output_array)
