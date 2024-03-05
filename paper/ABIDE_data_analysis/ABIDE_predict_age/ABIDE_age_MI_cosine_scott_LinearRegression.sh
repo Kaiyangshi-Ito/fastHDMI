e041216193633dee7f4d58e2c8e59d5ce2c90bb8
Kai Yang <yang_k@outlook.com>
2024-03-05 13:23:55 -0500

    change the python version used in ABIDE analysis

--- paper/ABIDE_data_analysis/ABIDE_predict_age/ABIDE_age_MI_cosine_scott_LinearRegression.sh
+++ paper/ABIDE_data_analysis/ABIDE_predict_age/ABIDE_age_MI_cosine_scott_LinearRegression.sh
@@ -6,14 +6,14 @@
 #SBATCH --time=3-12:00:00
 #SBATCH --job-name=age_MI_cosine_scott_LinearRegression
 
-module load arch/avx2 gcc llvm rust arrow cuda nodejs python/3.8.10 r/4.0.2 python-build-bundle
+module load arch/avx2 gcc/12.3 llvm/16.0.6 rust arrow cuda nodejs python/3.10 r/4.0.2 python-build-bundle/2023b
 
 virtualenv --no-download $SLURM_TMPDIR/env
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip Cython
 
 # ### run this block at the login node to build wheels
-# module load arch/avx2 gcc llvm rust arrow cuda nodejs python/3.8.10 r/4.0.2 python-build-bundle
+# module load arch/avx2 gcc/12.3 llvm/16.0.6 rust arrow cuda nodejs python/3.10 r/4.0.2 python-build-bundle/2023b
 # ### upgrading the tools
 # pip install --upgrade pip setuptools wheel
 # ### remove all old wheels
@@ -21,21 +21,21 @@ pip install --no-index --upgrade pip Cython
 # ### get wheels builder
 # git clone https://github.com/ComputeCanada/wheels_builder
 # export PATH=$PATH:${HOME}/wheels_builder
-# ### build KDEpy 1.1.5
-# ${HOME}/wheels_builder/unmanylinuxize.sh --package KDEpy --version 1.1.5 --python 3.8,3.9,3.10 --find_links https://files.pythonhosted.org/packages/
+# ### build KDEpy 1.1.9
+# ${HOME}/wheels_builder/unmanylinuxize.sh --package KDEpy --version 1.1.9 --python 3.8,3.9,3.10 --find_links https://files.pythonhosted.org/packages/
 # ### built nonconvexAG 1.0.6
 # ${HOME}/wheels_builder/unmanylinuxize.sh --package nonconvexAG --version 1.0.6 --python 3.8,3.9,3.10 --find_links https://files.pythonhosted.org/packages/
 # ### built dask 2024.2.1
 # ${HOME}/wheels_builder/unmanylinuxize.sh --package dask --version 2024.2.1 --python 3.8,3.9,3.10 --find_links https://files.pythonhosted.org/packages/
-# ### built fastHDMI 1.25.6
-# pip install fastHDMI==1.25.6 --no-cache-dir
+# ### built fastHDMI 1.25.16
+# pip install fastHDMI==1.25.16 --no-cache-dir
 # pip wheel fastHDMI --no-deps
 
 # # Here basically to build the packages at login node and install them in slurm job submission locally
 pip install --no-index bed-reader numpy sklearn matplotlib scipy numba multiprocess scikit-learn cupy rpy2 pandas dask Cython
-pip install --no-index /home/kyang/KDEpy-1.1.5+computecanada-cp38-cp38-linux_x86_64.whl
+pip install --no-index /home/kyang/KDEpy-1.1.9+computecanada-cp310-cp310-linux_x86_64.whl
 pip install --no-index /home/kyang/nonconvexAG-1.0.6+computecanada-py3-none-any.whl
-pip install --no-index /home/kyang/fastHDMI-1.25.6-cp38-cp38-linux_x86_64.whl
+pip install --no-index /home/kyang/fastHDMI-1.25.16-cp310-cp310-linux_x86_64.whl
 pip install --no-index /home/kyang/dask-2024.2.1+computecanada-py3-none-any.whl
 
 nvidia-smi
